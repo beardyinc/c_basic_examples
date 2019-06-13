066e3018c8a66b5d5f7e08fbd1c12e843be8f35c
Paul Latzelsperger <paul@beardyinc.com>
2019-06-13 15:13:09 +0200

    updated file io examples

--- BasicExamplesC/IO/FileIO.c
+++ BasicExamplesC/IO/FileIO.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #pragma warning(disable : 4996)
 
-void fileIOdemo(char* text) {
-	FILE *file= fopen("foo.txt", "w+");
+void writeFile(char* text, char* filename) {
+
+	FILE *file= fopen(filename, "w+");
 	fprintf(file, text);
 	fflush(file);
 	fclose(file);
+}
+
+char* readFile(char* filename) {
+
+	FILE* srcCode = fopen(filename, "r+");
+	if (srcCode == NULL) {
+		printf("Error opening file %s!", filename);
+		return NULL;
+	}
+
+	char* fileContents = malloc(512);
+	int filledItems = fscanf(srcCode, "%[^\n]s", fileContents);
+	fclose(srcCode);
+	return fileContents;
 }
--- BasicExamplesC/IO/IO.c
+++ BasicExamplesC/IO/IO.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #pragma warning(disable : 4996)
 
 // call function in other c file
-extern void fileIOdemo();
+extern void writeFile(char *, char* filename);
+extern const char* readFile(char*);
 
 void main() {
 
+	//read a file
+	const char* fileContents= readFile("c:\\Users\\codersbay\\Documents\\test2.txt");
+	printf("Contents of file: \n\n%s\n\n", fileContents);
 
+	free(fileContents);
+	fileContents = NULL;
+
+
+
+	//write a file
 	char buffer[100];
 	int read;
 
-	printf("enter a value: \n");
+	printf("enter a value that will be written to another file: \n");
 	read= scanf("%s", buffer);
 
 	printf("you entered %s\n", buffer);
-	printf("%d items successfully read\n", read);
 
 	printf("writing to file\n");
-	fileIOdemo(buffer);
+	writeFile(buffer, "foo.txt");
 }
